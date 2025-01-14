87bbab2d7133cc453d7ebf0484c2eac7bea34cb8
Adrien Piquerez <adrien.piquerez@gmail.com>
2025-01-14 14:34:03 +0100

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,16 +1,16 @@
 import sbt._
 
 object Dependencies {
-  val scala3Next = "3.4.2"
-  val asmVersion = "9.7"
-  val coursierVersion = "2.1.10"
+  val scala3Next = "3.6.2"
+  val asmVersion = "9.7.1"
+  val coursierVersion = "2.1.24"
 
-  val tastyQuery = "ch.epfl.scala" %% "tasty-query" % "1.3.0"
+  val tastyQuery = "ch.epfl.scala" %% "tasty-query" % "1.4.0"
   val asm = "org.ow2.asm" % "asm" % asmVersion
   val asmUtil = "org.ow2.asm" % "asm-util" % asmVersion
 
   // test dependencies
-  val munit = "org.scalameta" %% "munit" % "1.0.0"
+  val munit = "org.scalameta" %% "munit" % "1.0.4"
   val coursier = ("io.get-coursier" %% "coursier" % coursierVersion).cross(CrossVersion.for3Use2_13)
   val coursierJvm = ("io.get-coursier" %% "coursier-jvm" % coursierVersion).cross(CrossVersion.for3Use2_13)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,2 @@
-addSbtPlugin("com.github.sbt" % "sbt-ci-release" % "1.5.12")
-addSbtPlugin("org.scala-debugger" % "sbt-jdi-tools" % "1.1.1")
+addSbtPlugin("com.github.sbt" % "sbt-ci-release" % "1.9.2")
+addSbtPlugin("com.github.sbt" % "sbt-jdi-tools" % "1.2.0")
